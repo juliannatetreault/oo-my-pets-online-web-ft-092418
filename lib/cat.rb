fe467df36478aefc565b9c8e854eeaa6088f3ec1
Julianna Tetreault <juliannatetreault@icloud.com>
2018-10-09 03:44:22 +0000

    automatically backed up by learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,12 @@
 class Cat
-  # code goes here
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name 
+    #raise NoMethodError if name != "Crookshanks"
+  end
+  
+  def mood(mood)
+    
+  end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,63 @@
 class Owner
-  # code goes here
+  attr_accessor :name, :pets 
+  attr_reader :species
+  @@all = []
+  
+  def initialize(species)
+    @species = species
+    @pets = {fishes: [], cats: [], dogs: []}
+    @@all << self 
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    self.all.count 
+  end
+  
+  def self.reset_all 
+    self.all.clear 
+  end
+  
+  def species
+    @species
+  end
+  
+  def say_species 
+    "I am a #{@species}."
+  end
+  
+  def buy_fish(name)
+    @pets[:fishes] = [Fish.new(name)]
+  end
+  
+  def buy_cat(name)
+    @pets[:cats] = [Cat.new(name)]
+  end
+  
+  def buy_dog(name)
+    @pets[:dogs] = [Dog.new(name)]
+  end
+  
+  def walk_dogs
+    
+  end
+  
+  def play_with_cats
+    
+  end
+  
+  def feed_fish
+    
+  end
+  
+  def sell_pets
+    
+  end
+  
+  def list_pets
+    @pets 
+  end
 end
